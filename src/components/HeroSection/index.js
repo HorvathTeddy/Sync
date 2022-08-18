03f133527a844495e5a881708a2e03663746047a
Teddy Horvath <teddyhorvath@gmail.com>
2022-08-18 08:04:33 -0700

    HeroPage styling

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { HeroContainer, HeroBg, VideoBg,
+    HeroH1,
+    HeroP,
+    HeroBtnWrapper,
+    ArrowForward,
+    ArrowRight,
+    HeroContent
+        } from './HeroElements'
+import Video from '../../videos/video.mp4'
+import { Button } from '../ButtonElement'
 
 const HeroSection = () => {
+    const [hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
   return (
     <HeroContainer id='home'>
         <HeroBg>
             <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
         </HeroBg>
+        <HeroContent>
+            <HeroH1>
+                Music Collaboration Made Easy
+            </HeroH1>
+            <HeroP>
+                Create an account and start collaborating. First week is on us!
+            </HeroP>
+            <HeroBtnWrapper>
+                <Button to='/createaccount' onMouseEnter={onHover} onMouseLeave={onHover}>
+                    Get started {hover ? <ArrowForward /> : <ArrowRight />}
+                </Button>
+            </HeroBtnWrapper>
+        </HeroContent>
     </HeroContainer>
   )
 }
