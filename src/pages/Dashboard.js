4e8e804e19d47818514dc57c519728aeee08d89b
Teddy Horvath <teddyhorvath@gmail.com>
2023-07-08 12:58:27 -0400

    debug

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,9 +4,9 @@ import { useNavigate } from "react-router-dom";
 //import "./Dashboard.css";
 import { auth, db, logout } from "../firebase";
 import { query, collection, getDocs, where } from "firebase/firestore";
-import Navbar from "../components/Navbar";
+//import Navbar from "../components/Navbar";
 function Dashboard() {
-  const [user, loading, error] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
   const [name, setName] = useState("");
   const navigate = useNavigate();
   const fetchUserName = async () => {
